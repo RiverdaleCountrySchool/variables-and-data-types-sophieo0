e39d320af9d84e26425582511cc496b2d625f70c
sophieo0 <sotterman27@riverdale.edu>
2025-01-31 14:32:02 -0500

    Updated

--- src/DoubleDiscount.java
+++ src/DoubleDiscount.java
@@ -1,10 +1,17 @@
 public class DoubleDiscount {
     public static void main(String[] args) {
 
-        double num1 = 25.99;
-        double num2 = 0.15;
+        double num1 = Double.valueOf(args[0]);
+        double num2 = Double.valueOf(args[1]);
         double discountPrice = num1 - (num1*0.15);
         System.out.println("The sale price is " + discountPrice + ".");
+
+        /*
+        Integer.valueOf(s)
+        Double.valueOf(s)
+        Long.valueOf(s)
+        Boolean.valueOf(s)
+         */
 
     }
 }
--- src/EonsCalc.java
+++ src/EonsCalc.java
@@ -1,8 +1,8 @@
 public class EonsCalc {
     public static void main(String[] args) {
 
-        long ageOfUniverse = 13700000000L;
-        long yearsSinceDinosaurs = 66000000;
+        long ageOfUniverse = Long.valueOf(args[0]);//13700000000L
+        long yearsSinceDinosaurs = Long.valueOf(args[1]);//66000000
         System.out.print("The universe was " + yearsSinceDinosaurs + " years old when the dinosaurs went extinct.");
     }
 }
